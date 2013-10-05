dfb8bc9a1d9c5c12e5e67e6a3472876259e1523b
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-05 14:24:21 +0100

    Added constants file R.java
    Used to clean up the code a bit and get generally accessible constants.

---
+++ util/R.java
@@ -0,0 +1,11 @@
+package util;
+
+public class R {
+
+	public final static int TICK_TIME = 1000;
+	public final static int MIN_PLAYERS = 4;
+	
+	public final static int GAME_WAITING = 0;
+	public final static int GAME_LAUNCHING = 1;
+	
+}
