2a62a57f652ff4117637adee06db3dbbadf5032b
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-05 14:46:47 +0100

    GameAgent now waits for player with DF search.
    Added a subscription to the DF which calls recievedPlayers() everytime
    a new PlayerAgent is found. Then checks if the game should start
    depending on the number of players. searchPlayers() was deprecated and
    was deleted. Extracted constants to R.java.

--- agents/GameAgent.java
+++ agents/GameAgent.java
@@ -1,51 +1,113 @@
 package agents;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+import util.R;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
+import jade.proto.SubscriptionInitiator;
 
 public class GameAgent extends Agent {
-	int i = 0;
-	public final int TICK_TIME = 1000;
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8137655407030225340L;
-	
+
+	ArrayList<DFAgentDescription> players = new ArrayList<DFAgentDescription>();
+
+	int currentRound = 0;
+	int currentPlayer = 0;
+	int gameStatus = R.GAME_WAITING;
+
 	protected void setup() {
-		System.out.println("Game launching.");
-		addBehaviour(new RoundsBehaviour(this, TICK_TIME));
-		
-		
+		System.out.println("#############");
+		System.out.println("WAITING FOR PLAYERS");
+		System.out.println("#############");
+		//waitForPlayers();
+		subscribeDF();
+
 	}
-	
-	protected void searchPlayers() {
+
+	private void subscribeDF() {
+		// Build the description used as template for the subscription
 		DFAgentDescription template = new DFAgentDescription();
-		ServiceDescription sd1 = new ServiceDescription();
-		sd1.setType("player");
-		template.addServices(sd1);
-		try {
-			DFAgentDescription[] result = DFService.search(this, template);
-			// envia mensagem "pong" inicial a todos os agentes "ping"
-			ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
-			for(int i=0; i<result.length; ++i) {
-				msg.addReceiver(result[i].getName());	
-				System.out.println("Found player " + result[i].getName());
-			}
-			msg.setContent("alive");
-			send(msg);
-			
-		} catch(FIPAException e) {
-			e.printStackTrace();
+		ServiceDescription templateSd = new ServiceDescription();
+		templateSd.setType("player");
+		template.addServices(templateSd);
+
+		SearchConstraints sc = new SearchConstraints();
+		// We want to receive 10 results at most
+		//sc.setMaxResults(new Long(10));
+
+		addBehaviour(new SubscriptionBehaviour(this, DFService.createSubscriptionMessage(this, getDefaultDF(), template, sc)));
+
+	}
+
+	protected void receivedPlayers() {
+		// If there are the minimum players, start the game
+		if(players.size() >= R.MIN_PLAYERS) {
+			startGame();
 		}
 	}
+
+	protected void startGame() {
+		if(gameStatus != R.GAME_WAITING) {
+			return;
+		}
+		gameStatus = R.GAME_LAUNCHING;
+		// Shuffles the players to generate random playing order
+		System.out.println("#############");
+		System.out.println("SHUFFLING PLAYERS");
+		System.out.println("#############");
+
+		Collections.shuffle(players);
+
+		prettyPrintOrder();
+
+		System.out.println("#############");
+		System.out.println("GAME STARTING");
+		System.out.println("#############");
+
+		addBehaviour(new RoundsBehaviour(this, R.TICK_TIME));
+	}
 	
+	private void prettyPrintOrder() {
+//		System.out.println(players.get(0).getName().toString() + " gets to go first!");
+
+		for (DFAgentDescription player : players) {
+			System.out.println(player.getName().getLocalName() + "");
+		}
+
+	}
+
+	/**
+	 * Implements a game turn.
+	 */
+	protected void gameTurn() {
+
+		if(currentPlayer > players.size() - 1) {
+			currentPlayer = 0;
+		}
+
+		ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
+		msg.addReceiver(players.get(currentPlayer).getName());
+		msg.setContent("play");
+		send(msg);
+
+		currentPlayer++;
+	}
+
 	/**
 	 * Inner class RoundsBehaviour
 	 */
@@ -62,11 +124,43 @@ public class GameAgent extends Agent {
 
 		protected void onTick() {
 			// For each agent, send him a message to play and wait response or timeout.
-			System.out.println("Playing.." + i++);
-			searchPlayers();
-			
-			
+			System.out.println("Round " + currentRound++);
+			gameTurn();
 		}
 
 	}    // END of inner class RoundsBehaviour
+
+	/**
+	 * Inner class SubscriptionBehaviour
+	 */
+	private class SubscriptionBehaviour extends SubscriptionInitiator {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -214667283251111849L;
+
+		public SubscriptionBehaviour(Agent a, ACLMessage msg) {
+			super(a, msg);
+			// TODO Auto-generated constructor stub
+		}
+
+		protected void handleInform(ACLMessage inform) {
+
+			try {
+				DFAgentDescription[] results = DFService.decodeNotification(inform.getContent());
+				if (results.length > 0) {
+					for (int i = 0; i < results.length; ++i) {
+						players.add(results[i]);
+					}
+				}
+				// Notifies the game that new players are ready.
+				receivedPlayers();
+			}
+			catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+		}
+
+	} // END of inner class SubscriptionBehaviour
 }
